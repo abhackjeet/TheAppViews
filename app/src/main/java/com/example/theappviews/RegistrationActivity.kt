68ef89d2a6ab1e3b6abcd4a108c874d74d535dc9
Abhijeet Prakash <prakashabhijeet7@gmail.com>
2021-08-21 00:24:51 +0530

    added_fragment

--- app/src/main/java/com/example/theappviews/RegistrationActivity.kt
+++ app/src/main/java/com/example/theappviews/RegistrationActivity.kt
@@ -5,18 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.text.TextUtils
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.*
 import androidx.core.view.get
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_registration.*
 import java.lang.ref.PhantomReference
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_registration.genderTextView
+
+
+
+
 
 class RegistrationActivity : AppCompatActivity() {
+
+    lateinit var option : Spinner
+    lateinit var result : TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +36,23 @@ class RegistrationActivity : AppCompatActivity() {
         val nicknameInput = findViewById<EditText>(R.id.nicknameInput)
         val usernameInput = findViewById<EditText>(R.id.usernameInput)
         val passwordInput = findViewById<EditText>(R.id.passwordInput)
+        val spinnerInput = findViewById<Spinner>(R.id.spinnerOne)
+
+/*
+        val pets = arrayOf("Dog", "Cat", "Rabbit", "Horse", "Panda")
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, pets)
+
+        spinnerOne.adapter = arrayAdapter
+
+*/
+
+        option = findViewById(R.id.spinnerOne) as Spinner
+        result = findViewById(R.id.petsText) as TextView
+
+        val options = arrayOf("Option 1", "Option 2","Option 3")
+
+        option.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,options)
+
 
         registerButton.setOnClickListener {
 
@@ -55,8 +81,9 @@ class RegistrationActivity : AppCompatActivity() {
             val nickname = nicknameInput.text.toString()
             val emailId = usernameInput.text.toString()
             val password = passwordInput.text.toString()
-            val genderMale = male.text.toString()
-            val genderFemale = female.text.toString()
+            // val genderMale = male.text.toString()
+            // val genderFemale = female.text.toString()
+
 
 
             val intent = Intent(this@RegistrationActivity, ProfileActivity::class.java)
@@ -64,6 +91,7 @@ class RegistrationActivity : AppCompatActivity() {
             intent.putExtra("Nickname", nickname)
             intent.putExtra("User Email ID", emailId)
             intent.putExtra("Password", password)
+
 
             val id = radioGroup.checkedRadioButtonId
 
@@ -74,9 +102,27 @@ class RegistrationActivity : AppCompatActivity() {
                     Toast.LENGTH_LONG).show()
             }
 
+            option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    result.text = "Please Select an Option"
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    val selected: String = spinnerInput.getItemAtPosition(position).toString()
+                    intent.putExtra("Pet", selected)
+
+                }
+            }
+
             startActivity(intent)
 
         }
 
+
+
     }
+
+
+
+
 }
